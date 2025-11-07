6c92bbd1dbffa203e960272b02b0df0cc228dbe6
Clara Giovanna <Fmoura.silva27@gmail.com>
2025-11-07 11:55:59 -0300

    Show da tabela secretaria

--- database/disciplina_ddl.sql
+++ database/disciplina_ddl.sql
@@ -4,7 +4,7 @@ nome VARCHAR(200) NOT NULL,
 carga_horaria int NOT NULL,
 curso_id INT,
 FOREIGN KEY (curso_id) REFERENCES curso(id)
-);
+)ENGINE=MYISAM;
 
 INSERT INTO  disciplina (id, nome, carga_horaria, curso_id) VALUES
 (1, 'Desenvolvimento de sistemas web', 4, 1);
