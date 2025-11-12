3fdd192859c85dab24ef68888d171dc3db936727
Amanda Lopes <amanda.lopesoliveira999@gmail.com>
2025-11-12 09:02:41 -0300

    cordenado rotas

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,13 +33,7 @@
                             <a class="dropdown-item" href="{{ route('aluno.index') }}">Listar</a>
                         </div>
                     </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Cordenação</a>
-                        <div class="dropdown-menu">
-                            <a class="dropdown-item" href="{{ route('cordenador.create') }}">Cadastrar</a>
-                            <a class="dropdown-item" href="{{ route('cordenador.index') }}">Listar</a>
-                        </div>
-                    </li>
+                
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Curso</a>
                         <div class="dropdown-menu">
