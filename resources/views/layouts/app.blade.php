e96eda88777a1dbac9e58da4420de70af3c2943f
Arthur Moura <arthurmoura28030@gmail.com>
2025-11-12 07:58:49 -0300

    diretoria adicionado

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -76,6 +76,13 @@
                         </div>
                     </li>
                     <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Diretoria</a>
+                        <div class="dropdown-menu">
+                        <a class="dropdown-item" href="{{ route('diretoria.create') }}">Cadastrar</a>
+                        <a class="dropdown-item" href="{{ route('diretoria.index') }}">Listar</a>
+                        </div>
+                    </li>
+                    <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Professor</a>
                         <div class="dropdown-menu">
                         <a class="dropdown-item" href="{{ route('professor.create') }}">Cadastrar</a>
