1fb97a34e77cb8f42348a12d1d8cad2e701f67d7
Flavio Dantas <flas61522@gmail.com>
2025-11-12 07:48:37 -0300

    Edit de professor

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,8 +36,8 @@
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Professor</a>
                         <div class="dropdown-menu">
-                        <a class="dropdown-item" href="{{ route('aluno.create') }}">Cadastrar</a>
-                        <a class="dropdown-item" href="{{ route('aluno.index') }}">Listar</a>
+                        <a class="dropdown-item" href="{{ route('professor.create') }}">Cadastrar</a>
+                        <a class="dropdown-item" href="{{ route('professor.index') }}">Listar</a>
                         </div>
                     </li>
                     <li class="nav-item dropdown">
