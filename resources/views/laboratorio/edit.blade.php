8799d927d4079339a3a66346b4a9fd0900faf299
Enzo Gabriel dos Santos Lopes <enzog0765@gmail.com>
2025-11-12 11:01:02 -0300

    Desenvolvento Laboratorio Edit

---
+++ resources/views/laboratorio/edit.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.app')
+@section('title', 'Cadastrar Laboratório')
+
+@section('content')
+<div class="container">
+    <h1>Cadastrar Novo Laboratório</h1>
+
+    <form action="{{  route("laboratorio.update",$laboratorio->id) }}" method="POST" class="mt-4" enctype="multipart/form-data">
+        @csrf
+
+        <div class="mb-3">
+            <label for="nome" class="form-label">Nome do Laboratório</label>
+            <input type="text" name="nome" id="nome" class="form-control" placeholder="Digite o nome do laboratório" value="{{ $laboratorio->nome }}" required>
+        </div>
+
+        <div class="mb-3">
+            <label for="tipo" class="form-label">Tipo</label>
+            <input type="text" name="tipo" id="tipo" class="form-control" placeholder="Ex: Informática, Química, Robótica..." value="{{ $laboratorio->tipo }} required>
+        </div>
+
+        <div class="mb-3">
+            <label for="capacidade" class="form-label">Capacidade</label>
+            <input type="number" name="capacidade" id="capacidade" class="form-control" placeholder="Quantidade máxima de pessoas" value="{{ $laboratorio->capacidade }} required>
+        </div>
+
+        <div class="mb-3">
+            <label for="equipmentos" class="form-label">Equipamentos</label>
+            <textarea name="equipamentos" id="equipamentos" class="form-control" rows="3" placeholder="Liste os principais equipamentos disponíveis" value="{{ $laboratorio->equipamentos }}></textarea>
+        </div>
+
+        <div class="mb-3">
+            <label for="responsavel" class="form-label">Responsável</label>
+            <input type="text" name="responsavel" id="responsavel" class="form-control" placeholder="Nome do responsável pelo laboratório" value="{{ $laboratorio->responsavel }}" required>
+        </div>
+
+        <div class="d-flex justify-content-between">
+            <a href="{{ route('laboratorio.index') }}" class="btn btn-secondary">Voltar</a>
+            <button type="submit" class="btn btn-primary">Salvar</button>
+        </div>
+    </form>
+</div>
+@endsection
