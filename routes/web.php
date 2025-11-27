803ba0b9dda62faf54f8502d6b05bc9d08f0f703
Flavio Dantas <flas61522@gmail.com>
2025-11-27 08:57:42 -0300

    remoção rotas

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,10 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AlunoController;
-use App\Http\Controllers\CursoController;
-use App\Http\Controllers\TurmaController;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\DiretoriaController;
-use App\Http\Controllers\ProfessorController;
-use App\Models\Professor;
-use App\Http\Controllers\AvaliacaoController;
-use App\Http\Controllers\LaboratorioController;
-use App\Http\Controllers\ZeladoriaController;
-use App\Http\Controllers\MonitorController;
+use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\DisciplinaController;
-use App\Http\Controllers\SecretariaController;
-use App\Http\Controllers\CoordenadorController;
-use App\Http\Controllers\EquipamentoController;
-use App\Http\Controllers\SalaController;
-
-use App\Http\Controllers\PorteiroController;
-
-Route::get('/', [HomeController::class, 'listarPublicacoes'])->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -32,31 +14,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::resource('curso', CursoController::class);
-    Route::resource('turma', TurmaController::class);
-    Route::resource('diretoria', DiretoriaController::class);
-    Route::resource('professor', ProfessorController::class);
-    Route::resource('laboratorio', LaboratorioController::class);
-    Route::resource('zeladoria', ZeladoriaController::class);
-    Route::resource('aluno', AlunoController::class);
-    Route::get('/aluno/{id}/boleto', [AlunoController::class, 'boleto'])->name('aluno.boleto');
 
-    Route::get('/laboratorio/{id}/edit', [LaboratorioController::class, 'edit'])
-    ->name('laboratorio.edit');
-    Route::resource('disciplina', DisciplinaController::class);
-    Route::resource('secretaria', SecretariaController::class);
-    Route::resource('diretoria', DiretoriaController::class);
-    Route::resource('monitores', MonitorController::class);
-    Route::resource('coordenador', CoordenadorController::class);
-    Route::resource('equipamento', EquipamentoController::class);
-    Route::resource('porteiro', PorteiroController::class);
-    Route::resource('sala', SalaController::class);
 });
 
-Route::post('cadastrar/turma', [TurmaController::class, 'cadastrar']);
-Route::post('/like', [AvaliacaoController::class, 'like'])->name('like');
-Route::get('consultar/turma/{id}', [TurmaController::class, 'consultar']);
-
 require __DIR__ . '/auth.php';
-    Route::resource('monitoria', MonitorController::class);
-}
