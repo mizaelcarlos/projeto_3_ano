8839b9f3266859d653898d9494e683a6ff2f0651
Enzo Gabriel dos Santos Lopes <enzog0765@gmail.com>
2025-11-28 10:23:08 -0300

    mudando

---
+++ app/Http/Controllers/GraficoController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use ArielMejiaDev\LarapexCharts\LarapexChart;
+
+class GraficoController extends Controller
+{
+    public function grafico1 (){
+    $chart = (new LarapexChart)->setTitle('Posts')
+        ->setDataset([150, 120])
+        ->setLabels(['Published', 'No Published']);
+
+    return view('grafico', compact('chart'));
+    }
+}
--- resources/views/teste1.blade.php
+++ resources/views/teste1.blade.php
@@ -4,6 +4,7 @@
 
 <div class="container">
     <h1>QR Code</h1>
+    {{!! QrCode::size(200)->generate('Enzo Gabriel do Guiguiba') !!}}
 </div>
 
 @endsection
