4b5dbe1af125e268a085d9a1a2aa0c81b31a0830
Maria Eduarda <dudapedreira2008@gmail.com>
2025-11-28 08:07:51 -0300

    ajustes locais

---
+++ resources/views/teste1.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+@section('title', ' TesteQRCode')
+@section('content')
+
+<div class="container">
+
+
+</div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,16 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TesteQRCodeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
-<<<<<<< HEAD
-Route::get('/dashboard', [HomeController::class, 'principal'],  function () {
-=======
 Route::get('/portfolio/create', [HomeController::class, 'create']);
 
 Route::post('/portfolio/cadastrar/{id}', [HomeController::class, 'store'])->name('portfolio.cadastrar');
 
 Route::get('/dashboard', function () {
->>>>>>> d99e79fdf9417af0413c17e763d313decd5ed156
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
@@ -22,7 +19,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
 });
+
+Route ::get('testeqrcode', [TesteQRCodeController::class, 'teste1']);
 
 require __DIR__ . '/auth.php';
