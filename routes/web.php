739fafb4e8216775920cdc455b560734fd38e6ce
Antonio Guilherme <antonioguilhermesilvag1@gmail.com>
2025-11-28 07:34:55 -0300

    Correção

--- routes/web.php
+++ routes/web.php
@@ -6,15 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
-<<<<<<< HEAD
 Route::get('/dashboard', [HomeController::class, 'principal'],  function () {
-=======
-Route::get('/portfolio/create', [HomeController::class, 'create']);
-
-Route::post('/portfolio/cadastrar/{id}', [HomeController::class, 'store'])->name('portfolio.cadastrar');
-
-Route::get('/dashboard', function () {
->>>>>>> d99e79fdf9417af0413c17e763d313decd5ed156
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
