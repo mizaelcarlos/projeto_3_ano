03ccf9b7db123035005e6877cdb788f28c28ab16
Mirelle Lopes <mirellelopess200@gmail.com>
2025-11-28 08:00:02 -0300

    mudando

---
+++ resources/views/teste1.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+@section('title', ' Teste QRCode')
+@section('content')
+
+<div class="container">
+
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TesteQRController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
@@ -16,5 +17,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
 });
+Route::get(uri: 'testeqrcode', action: [TesteQRController::class, 'teste1']);
 
 require __DIR__ . '/auth.php';
